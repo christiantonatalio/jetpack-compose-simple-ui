4ba2c549ecaf619eeb65a7376721a36e4d60bde6
natal <christianto.natalio.saleky@mail.ugm.ac.id>
2021-10-09 22:44:36 +0700

    another commit

--- app/src/main/java/com/example/furee/ui/Appointment.kt
+++ app/src/main/java/com/example/furee/ui/Appointment.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +31,6 @@ object PaymentOptions{
 fun Appointment() {
     val scrollState = rememberScrollState()
 
-    // Smooth scroll to specified pixels on first composition
     LaunchedEffect(Unit) { scrollState.animateScrollTo(10000) }
 
         Column (modifier = Modifier
